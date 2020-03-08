39973cc2d9c4acc5d16eba42d4c6b15266266cf2
mateus rodrigues <mateus037@gmail.com>
2020-03-08 15:10:35 -0300

    Validando a sessão de usuários

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -9,15 +9,25 @@ destroy: quando queremos deletar uma sessao
 */
 
 import User from '../models/User'
+import * as Yup from 'yup'
+import { Schema, SchemaType } from 'mongoose'
+
 class SessionController {
 
-   async store(req, res) {
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required()
+        })
         const { email } = req.body
 
-        let user = await User.findOne({email})//encontra se há uma registro com essas informações
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha na validação.' })
+        }
+
+        let user = await User.findOne({ email })//encontra se há uma registro com essas informações
 
         //Cria um registro caso não exista essas informações
-        if(!user){
+        if (!user) {
             user = await User.create({ email })
         }
 
